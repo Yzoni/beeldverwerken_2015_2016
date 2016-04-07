46a81f095ec3c5e7f3fc352370cb7f0c01a81333
Jeroen Terstall <jeroen_t@hotmail.com>
2016-04-07 11:28:56 +0200

    linear method working

--- pixelValue.m
+++ pixelValue.m
@@ -2,7 +2,7 @@ function [ color ] = pixelValue( image, x, y, method, borderMethodName )
 %
 imageSize = size(image);
 if not(inImage(imageSize, x, y))
-    [newX, newY] = borderMethod( x, y, borderMethodName );
+    [newX, newY] = borderMethod( imageSize, x, y, borderMethodName );
 else
     newX = x;
     newY = y;
@@ -15,9 +15,9 @@ switch( method )
 
         case 'linear'
             % TODO a and b
-            a = newX - floor(newX);
-            b = newY - floor(newY);
-            color = (1 - a) * (1 - b) * image( floor(newX), floor(y) ) + ...
+            a = ceil(newX) - floor(newX);
+            b = (1 + newX)* floor(newX) - newX * ceil(newX);
+            color = (1 - a) * (1 - b) * image( floor(newX), floor(newY) ) + ...
                     (1 - a) * b * image( ceil(newX), floor(newY) ) + ...
                     a * b * image( floor(newX), ceil(newY) ) + ...
                     a * (1 - b) * image( ceil(newX), ceil(newY) );
