7844a442effacdc9fd822532220b79458b450b90
Jeroen Terstall <jeroen_t@hotmail.com>
2016-04-07 11:31:10 +0200

    linear method working

--- borderMethod.m
+++ borderMethod.m
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-function [ newX, newY ] = borderMethod( imageSize, x, y , borderMethod )
-=======
 function [ newX, newY ] = borderMethod( imageSize, x, y, borderMethod )
->>>>>>> 226b398b08f305aa44eaa9d4d0acf6ca18247c7c
 switch( borderMethod )
 	case 'constant_border'
 		newX = 100;
@@ -29,33 +25,19 @@ switch( borderMethod )
 
 	case 'periodic_border'
 		if (x > imageSize(1)) | (x < 1)
-<<<<<<< HEAD
 			newX = mod(x, imageSize(1));
             if newX < 1
                 newX = 1;
             end
-=======
-			newX = mod(x, imageSize(1))
-			if newX < 1
-				newX = 1;
-			end
->>>>>>> 226b398b08f305aa44eaa9d4d0acf6ca18247c7c
 		else
 			newX = x;
 		end
 		
 		if (y > imageSize(2)) | (y < 1)	
-<<<<<<< HEAD
 			newY = mod(y, imageSize(2));
             if newY < 1
                 newY = 1;
             end
-=======
-			newY = mod(y, imageSize(2))
-				if newY < 1
-					newY = 1;
-				end
->>>>>>> 226b398b08f305aa44eaa9d4d0acf6ca18247c7c
 		else
 			newY = y;
 		end
