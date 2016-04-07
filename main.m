9d18e8b2eee0c854c61e30fd58c73c0aefc95b99
Jeroen Terstall <jeroen_t@hotmail.com>
2016-04-07 13:20:56 +0200

    myAffine

--- main.m
+++ main.m
@@ -8,11 +8,20 @@ image = im2double( image );
 %pixelValue( image, 20, 20, 'nearest', 'nearest_border' );
 %pixelValue( image, 20, 20, 'linear', 'constant_border' );
 
-% plot ( profile ( image , 100 , 100 , 120 , 120 , 100 , 'nearest', 'nearest_border') );
+% plot ( profile 
+%( image , 100 , 100 , 120 , 120 , 100 , 'nearest', 'nearest_border') );
 % hold on;
 % plot ( profile ( image , 100 , 100 , 120 , 120 , 100 , 'linear', 'nearest_border') );
 
-tic;
-imshow( rotateImage( image, (pi/6), 'linear', 'periodic_border' ) )
+%tic;
+%figure(1)
+%imshow( rotateImage( image, (pi/6), 'linear', 'periodic_border' ) )
 
-toc
+%toc
+%tic;
+%figure(2)
+%imshow( rotateImage( image, (pi/6), 'nearest', 'periodic_border' ) )
+
+%toc
+
+imshow(myAffine ( image , 1 , 100 , 10 , 100 , 20 , 200 , 500 , 500 , 'linear', 'periodic_border'))
--- myAffine.m
+++ myAffine.m
@@ -1,10 +1,28 @@
-function [ r ] = myAffine ( image , x1 , y1 , x2 , y2 , x3 , y3 , M , N , method )
+    function [ r ] = myAffine ( image , x1 , y1 , x2 , y2 , x3 , y3 , M , N , method, borderMethodName )
 r = zeros (N , M ); % allocate new image of correct size
-% calculate X ( insert code for this )
+
+[imageSizeX, imageSizeY] = size(image);
+imageCenterX = round(imageSizeX / 2);
+imageCenterY = round(imageSizeY / 2);
+
+% Create the necessary rotation matrix
+phi = pi/4;
+t1 = imageCenterY;
+t2 = imageCenterX;
+rMatrix = [(cos(phi)) (-sin(phi)) t1; (sin(phi)) (cos(phi)) t2; 0 0 1];
+t1Translate = [1 0 -t1; 0 1 -t2; 0 0 1];
+
+% Transformed points
+newPoint = rMatrix * t1Translate * [x1 x2 x3; y1 y2 y3; 1 1 1];
+
+A = [x1, x2, x3; y1, y2, y3; 1, 1, 1]
+B = [newPoint(1,1), newPoint(1,2), newPoint(1,3); newPoint(2,1), newPoint(2,2), newPoint(2,3)]
+X = B / A
+
 for xa = 1: M
 	for ya = 1: N
-		% calculate x and y ( insert code for this )
-		r ( ya , xa ) = pixelValue ( image , x , y , method );
+        newPoint = X * [xa; ya; 1];
+		r ( ya , xa ) = pixelValue ( image , newPoint(1) , newPoint(2) , method, borderMethodName );
 	end
 end
 
