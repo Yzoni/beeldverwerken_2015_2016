1b1d5074aa0253c9b2302c867d81588e499e9839
Yzoni <yzonix@gmail.com>
2016-04-10 21:56:05 +0200

    restructure main

--- main.m
+++ main.m
@@ -1,31 +1,109 @@
 clear;
 clc;
 
-image = imread( 'res/flyers.png' );
-image = imread( 'res/cameraman.jpg' );
-image = rgb2gray( image );
-image = im2double( image );
+image_camera = imread( 'res/cameraman.jpg' );
+image_camera = rgb2gray( image );
+image_camera = im2double( image );
 
-%pixelValue( image, 20, 20, 'nearest', 'nearest_border' );
-%pixelValue( image, 20, 20, 'linear', 'constant_border' );
+image_flyers = imread( 'res/flyers.png' );
+image_flyers = rgb2gray( image );
+image_flyers = rgb2gray( image );
 
-% plot ( profile 
-%( image , 100 , 100 , 120 , 120 , 100 , 'nearest', 'nearest_border') );
-% hold on;
-% plot ( profile ( image , 100 , 100 , 120 , 120 , 100 , 'linear', 'nearest_border') );
+
+%%%%%%%%%%%%%%
+% Question 2 %
+%%%%%%%%%%%%%%
+
+%%%%%%%%%%%
+% 2 a, b, c)
+%%%%%%%%%%%
+pixelValue( image_camera, 20, 20, 'nearest', 'nearest_border' )
+pixelValue( image_camera, 20, 20, 'linear', 'constant_border' )
+
+hold on;
+plot ( profile ( image_camera , 100 , 100 , 120 , 120 , 100 , 'nearest', 'nearest_border') );
+plot ( profile ( image_camera , 100 , 100 , 120 , 120 , 100 , 'linear', 'nearest_border') );
+hold off;
+
+%%%%%
+% 2 d)
+%%%%%
+% Three different methods were used to try to solve the border problem. 
+% (1) The first solution is a constant border color. (2) The second solution is 
+% using expanding the most outer pixel outside of the border, the nearest 
+% border solution. (3) The third solution is using a periodic border, the image
+% is repeated and placed on the sides of the image.
+hold on;
+plot ( profile ( image_camera , 100 , 100 , 120 , 120 , 100 , 'nearest', 'nearest_border') );
+plot ( profile ( image_camera , 100 , 100 , 120 , 120 , 100 , 'linear', 'nearest_border') );
+hold off;
+% TODO COMPARISON
+
+
+%%%%%%%%%%%%%%
+% Question 3 %
+%%%%%%%%%%%%%%
+
+%%%%%%%%%%%
+% 3 a b & d)
+%%%%%%%%%%%
+imshow( rotateImage( image_camera, (pi/6), 'linear', 'periodic_border' ) )
+% TODO REMOVE LOOPS
+% TODO BETTER RESIZE
+
+%%%%%
+% 3 c)
+%%%%%
+tic;
+rotateImage( image_camera, (pi/6), 'nearest', 'nearest_border' );
+nearest_time = toc
 
 tic;
-%figure(1)
-%imshow( rotateImage( image, (pi/6), 'linear', 'periodic_border' ) )
+rotateImage( image_camera, (pi/6), 'linear', 'nearest_border' );
+linear_time = toc
 
-%toc
-%tic;
-%figure(2)
-%imshow( rotateImage( image, (pi/6), 'linear', 'periodic_border' ) )
+%%%%%
+% 3 d)
+%%%%%
+nearest_distance = distanceMeasure(image_camera, 'nearest')
+linear_distance = distanceMeasure(image_camera, 'linear')
 
-%toc
 
-%imshow(myAffine ( image , 1 , 357 , 357 , 357 , 357 , 1 , 500 , 500 , 'linear', 'periodic_border'))
-imshow(image)
+%%%%%%%%%%%%%%
+% Question 4 %
+%%%%%%%%%%%%%%
+
+%%%%%%%%%
+% 4 a b c)
+%%%%%%%%%
+x1 = 0;
+y1 = 0;
+x2 = 0;
+y2 = 0;
+x3 = 0;
+y3 = 0;
+imshow( myAffine(image_camera, x1, y1, x2, y2, x3, y3, 500, 500, 'linear', 'nearest_border') )
+% TODO REMOVE LOOPS
+% TODO SET SPECIFIC PARAMETERS
+
+
+%%%%%%%%%%%%%%
+% Question 5 %
+%%%%%%%%%%%%%%
+
+%%%%%%%%%%%
+% 5 a b c d)
+%%%%%%%%%%%
+imshow(image_flyers)
 [x, y] = ginput(4);
 imshow( myProjection(image, x(1), y(1), x(2), y(2), x(3), y(3), x(4), y(4), 500, 500, 'linear', 'constant_border') )
+% TODO REMOVE LOOPS
+
+
+%%%%%%%%%%%%%%
+% Question 7 %
+%%%%%%%%%%%%%%
+
+%%%%%%%%%%%
+% 7 a b c d)
+%%%%%%%%%%%
